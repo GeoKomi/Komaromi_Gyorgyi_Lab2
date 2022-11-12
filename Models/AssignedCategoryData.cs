f43b0b2d27906d4a0ec3ca4910effdaed246b829
Gyorgyi <k.gyorgyi05@gmail.com>
2022-11-12 19:38:56 +0200

    Lab1

---
+++ Models/AssignedCategoryData.cs
@@ -0,0 +1,9 @@
+﻿namespace Komaromi_Gyorgyi_Lab2.Models
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
